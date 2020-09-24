64f618cab9d4dacbae6fc727127a10c55a52dfbd
haziq <haziq.othman99@gmail.com>
2020-09-24 15:20:15 +0800

    Web-function

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "./App.css";
+import Row from "./Row";
+import request from "./requests";
+import Banner from "./Banner"
+
+function App() {
+  return (
+    <div className="app">
+      <Banner />
+      <Row title="NETFLIX ORIGINALS" fetchUrl={request.fetchNetflixOriginals}
+        isLargeRow={true} />
+      <Row title="Trending Now" fetchUrl={request.fetchTrending} />
+      <Row title="Top Rated" fetchUrl={request.fetchTopRated} />
+      <Row title="Action Movies" fetchUrl={request.fetchActionMovies} />
+      <Row title="Comedy Movies" fetchUrl={request.fetchComedyMovies} />
+      <Row title="Horror Movies" fetchUrl={request.fetchHorrorMovies} />
+      <Row title="Romance Movies" fetchUrl={request.fetchRomanceMovies} />
+      <Row title="Documentaries" fetchUrl={request.fetchDocumentaries} />
+    </div>
+  );
+}
+
+export default App;
